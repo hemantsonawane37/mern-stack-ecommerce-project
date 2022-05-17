8d1c9007945125cd746b75c603433453741aefcf
hemant sonawane <hemantsonawanem@gmail.com>
2022-05-17 15:34:17 +0530

    edit cors

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const path = require("path");
 
 
 const corsOptions = {
-    origin: ['http://localhost:3000','http://192.168.0.5:3000'] , 
+    origin: "*",
     credentials:true,  
     optionSuccessStatus:200 // For legacy browser support
 }
