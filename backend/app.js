0a9044b392dfd039bcc9af90c3015d5ee49aa982
hemant sonawane <hemantsonawanem@gmail.com>
2022-05-21 16:29:33 +0530

    .

--- backend/app.js
+++ backend/app.js
@@ -30,6 +30,7 @@ app.use("/api/v1", UserR);
 app.use("/api/v1", OrderR);
 
 app.use(express.static(path.join(__dirname,'../frontend/build')));
+
 app.get("*",(req,res)=> {
  res.sendFile(path.join(__dirname,"../frontend/build/index.html"))
 })
